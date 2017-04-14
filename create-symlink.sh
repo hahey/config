5b88e5b38d821f2aa78a26142346894ef7fd9e6c
hahey <heynaheyna9@gmail.com>
2017-04-14 15:26:21 +0200

    zsh config

--- create-symlink.sh
+++ create-symlink.sh
@@ -3,10 +3,16 @@
 VIM="/vim"
 VIMRC="/vimrc"
 PROFILE="/profile"
+ZSHRC="/zshrc"
+ZSHLOC="/zshrc.local"
 
 rm -rf ~/.vim
 ln -s $PWD$VIM ~/.vim
 ln -s $PWD$VIMRC ~/.vimrc
+
+ln -s $PWD$ZSHRC ~/.zshrc
+ln -s $PWD$ZSHLOC ~/.zshrc.local
+
 mkdir -p ~/.local/share/konsole
 ln -s $PWD$PROFILE ~/.local/share/konsole/profile
 cd vim
